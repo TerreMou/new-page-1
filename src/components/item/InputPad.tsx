87629377084063db4153146c115b32a749294e32
QiluMou <qmou17@gmail.com>
2022-12-10 19:00:09 +0800

    使用vantui的日期选择器&封装time.tsx

---
+++ src/components/item/InputPad.tsx
@@ -0,0 +1,77 @@
+import { defineComponent, ref } from 'vue'
+import { Icon } from '../../shared/Icon'
+import s from './InputPad.module.scss'
+import { time } from '../../shared/time'
+import { Popup, DatetimePicker } from 'vant'
+
+export const InputPad = defineComponent({
+  setup: (props, context) => {
+    const now = new Date()
+    const refDate = ref<Date>(now)
+    const refDatePickerVisible = ref(false)
+    const refAmount = ref('0')
+
+    const showDatePicker = () => {
+      refDatePickerVisible.value = true
+    }
+    const hideDatePicker = () => {
+      refDatePickerVisible.value = false
+    }
+    const setDate = (val: Date) => {
+      refDate.value = val
+      hideDatePicker()
+    }
+
+    const appendText = (n: number | string) => {
+      refAmount.value += n.toString()
+    }
+    const buttons = [
+      { text: '1', onClick: () => appendText(1) },
+      { text: '2', onClick: () => appendText(2) },
+      { text: '3', onClick: () => appendText(3) },
+      { text: '4', onClick: () => appendText(4) },
+      { text: '5', onClick: () => appendText(5) },
+      { text: '6', onClick: () => appendText(6) },
+      { text: '7', onClick: () => appendText(7) },
+      { text: '8', onClick: () => appendText(8) },
+      { text: '9', onClick: () => appendText(9) },
+      { text: '0', onClick: () => appendText(0) },
+      { text: '.', onClick: () => appendText('.') },
+      { text: '清空', onClick: () => {} },
+      { text: '提交', onClick: () => {} },
+    ]
+
+    return () => (
+      <>
+        <div class={s.dateAndAmount}>
+          <span class={s.date}>
+            <Icon class={s.icon} name="date" />
+            <span>
+              <span onClick={showDatePicker}>
+                {time(refDate.value).format()}
+              </span>
+              <Popup
+                position="bottom"
+                v-model:show={refDatePickerVisible.value}
+              >
+                <DatetimePicker
+                  value={refDate.value}
+                  type="date"
+                  title="选择年月日"
+                  onConfirm={setDate}
+                  onCancel={hideDatePicker}
+                />
+              </Popup>
+            </span>
+          </span>
+          <span class={s.amount}>{refAmount.value}</span>
+        </div>
+        <div class={s.buttons}>
+          {buttons.map((button) => {
+            return <button onClick={button.onClick}>{button.text}</button>
+          })}
+        </div>
+      </>
+    )
+  },
+})
--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -1,7 +1,9 @@
 import { defineComponent, ref } from 'vue'
+import s from './ItemCreate.module.scss'
 import { MainLayout } from '../../layout/mainLayout'
 import { Icon } from '../../shared/Icon'
 import { Tab, Tabs } from '../../shared/Tabs'
+import { InputPad } from './InputPad'
 
 export const ItemCreate = defineComponent({
   setup: (props, context) => {
@@ -12,12 +14,13 @@ export const ItemCreate = defineComponent({
           icon: () => <Icon name="left"></Icon>,
           default: () => (
             <>
-              <Tabs
-                v-model:selected={refKind.value}
-              >
+              <Tabs v-model:selected={refKind.value}>
                 <Tab name="支出">outcome...</Tab>
                 <Tab name="收入">income...</Tab>
               </Tabs>
+              <div class={s.inputPad_wrapper}>
+                <InputPad />
+              </div>
             </>
           ),
         }}
